57ff4825d6d8bf2736096b0b2b1b2786d2a6ba9f
IAMMDDANISHEQBAL <md.danish.eqbal.fiem.csds20@teamfuture.in>
2024-04-13 17:05:57 +0530

    book recommendation

--- tunes/views.py
+++ tunes/views.py
@@ -1,11 +1,14 @@
+import requests
+
 from django.shortcuts import render
-from django.http import HttpResponse
 from .ml_model import sentiment_predictor
+<<<<<<< Updated upstream
 from .speech import speech_to_text
+=======
+from .ml_model import fetch_books
+>>>>>>> Stashed changes
 
-
-# Create your views here.
-
+from django.http import HttpResponse
 def home(request):  
     return render(request, "home.html")
 
@@ -16,6 +19,7 @@ def recommendation(request):
     if request.method == 'GET':
         user_input = request.GET.get('text_input', '')
         result = sentiment_predictor([user_input])
+<<<<<<< Updated upstream
         return render(request, "recommendation.html", {'result': result})
     
 # def voice_data(request):
@@ -23,3 +27,25 @@ def recommendation(request):
 #         user_input = speech_to_text()
 #         result = sentiment_predictor([user_input])
 #         return render(request, "recommendation.html", {'result': result})
+=======
+
+        # Mapping of mood to book categories
+        mood_to_category = {
+            "Surprise": "comedy",
+            "Love": "romance",
+            "Joy": "adventure",
+            "Fear": "horror",
+            "Anger": "motivational",
+            "Sadness": "inspirational"
+        }
+
+        mood = result.get('mood', '').capitalize()  # Get the recognized mood and capitalize it
+        book_category = mood_to_category.get(mood, '')  # Get the corresponding book category
+
+        books = None
+        if book_category:
+            books = fetch_books(book_category)
+
+        return render(request, "recommendation.html", {'result': result, 'books': books})
+
+>>>>>>> Stashed changes
